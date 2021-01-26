f693de6d768ef2588c8bb9e9973343431cf8bcd4
yamee-dev <ryo.eiyama@icloud.com>
2021-01-26 23:04:07 +0900

    ロード時の処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,16 @@ const App = () => {
         getProducts
     );
     console.log(data);
+
+    const getTotalItems = () => null;
+
+    const handleAddToCart = () => null;
+
+    const handleRemoveFromCart = () => null;
+
+    if (isLoading) return <LinearProgress />;
+    if (error) return <div>Something went wrong ...</div>;
+
     return <div className="App">Start</div>;
 };
 
